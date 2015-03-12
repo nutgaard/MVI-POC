cecaecb69a50ee0891ee1eb9d3cdd8de2d7c1c32
Nicklas Utgaard <nutgaard@gmail.com>
2015-03-12 21:04:49 +0100

    Refactoring Model and Intent to use a common Component setup. Moved eventhandler creation into Component constructor to enable Component scoped testing.

--- src/Fluxin.js
+++ src/Fluxin.js
@@ -2,33 +2,19 @@ var React = require('react');
 var F = require('f-js').F;
 var assign = Object.assign || require('object.assign');
 
-function createEventHandler() {
-    var routes = this.getEventRouter();
-    var dummy = function(){};
-    this.handleEvent = function(event){
-        (routes[event.name] || dummy).bind(this)(event.payload);
-    }.bind(this);
-}
-function getOutput() {
-    if (this.outputStream === undefined) {
-        this.outputStream = F.stream();
+
+function createView(Model, Intent, options) {
+    if (!options.hasOwnProperty('mixins')) {
+        options.mixins = [];
     }
-    return this.outputStream;
-}
-function push() {
-    this.getOutput().push.apply(this.getOutput(), arguments);
-}
-function namedEvent(name) {
-    return function(event) {
-        this.push({
-            name: name,
-            payload: event
-        });
-    }.bind(this);
+
+    options.mixins.push(createMixin(Model, Intent));
+
+    return React.createClass(options);
 }
 
 function createMixin(Model, Intent) {
-    var mi = {
+    return {
         componentWillMount: function () {
             //0. Part of init View
             //Creating and maintained some information about Model and Intent
@@ -39,10 +25,8 @@ function createMixin(Model, Intent) {
             //The initialized model and intent
             //1. Init Intent
             this.fluxin.intent = new Intent();
-            createEventHandler.bind(this.fluxin.intent)();
             //2. Init Model
             this.fluxin.model = new Model();
-            createEventHandler.bind(this.fluxin.model)();
 
             //3a. Creating View stream
             //The output stream from the view. This stream is consumed by the intent.
@@ -61,7 +45,7 @@ function createMixin(Model, Intent) {
             this.fluxin.intent.getOutput().then(this.fluxin.model.handleEvent.bind(this.fluxin.model));
 
             //5. Creation of Model stream and connection to View handler
-            this.fluxin.model.getOutput().then(function() {
+            this.fluxin.model.getOutput().then(function () {
                 this.setState.apply(this, arguments);
             }.bind(this));
 
@@ -71,8 +55,6 @@ function createMixin(Model, Intent) {
             this.setState(initialModalState);
         }
     };
-    return mi;
-
 }
 
 function createModel(options) {
@@ -86,32 +68,15 @@ function createModel(options) {
             return {};
         }
     };
-
-    var workingOptions = assign({}, defaultOptions, options);
-
-    function Model(){}
-    Model.prototype.getOutput = getOutput;
-    Model.prototype.push = push;
-    Model.prototype.setState = function(newState){
+    var Model = createComponent(options, defaultOptions);
+    Model.prototype.setState = function (newState) {
         this.state = assign(this.state, newState);
         this.push(newState);
     };
 
-    for (option in workingOptions) {
-        Model.prototype[option] = workingOptions[option];
-    }
-
     return Model;
 }
-function createView(Model, Intent, options) {
-    if (!options.hasOwnProperty('mixins')) {
-        options.mixins = [];
-    }
 
-    options.mixins.push(createMixin(Model, Intent));
-
-    return React.createClass(options);
-}
 
 function createIntent(options) {
     var defaultOptions = {
@@ -119,18 +84,11 @@ function createIntent(options) {
             return {};
         }
     };
-    var workingOptions = assign({}, defaultOptions, options);
 
-    function Intent(){}
-    Intent.prototype.getOutput = getOutput;
-    Intent.prototype.push = push;
-    Intent.prototype.pushNamed = function(name, payload) {
+    var Intent = createComponent(options, defaultOptions);
+    Intent.prototype.pushNamed = function (name, payload) {
         namedEvent.bind(this)(name)(payload);
     };
-
-    for (option in workingOptions) {
-        Intent.prototype[option] = workingOptions[option];
-    }
 
     return Intent;
 }
@@ -140,3 +98,46 @@ module.exports = {
     createView: createView,
     createIntent: createIntent
 };
+
+function createComponent(options, defaultOptions) {
+    var workingOptions = assign({}, defaultOptions, options);
+
+    function Component() {
+        createEventHandler.bind(this)();
+    }
+
+    Component.prototype.getOutput = getOutput;
+    Component.prototype.push = push;
+
+    for (option in workingOptions) {
+        Component.prototype[option] = workingOptions[option];
+    }
+
+    return Component;
+}
+
+function createEventHandler() {
+    var routes = this.getEventRouter();
+    var dummy = function () {
+    };
+    this.handleEvent = function (event) {
+        (routes[event.name] || dummy).bind(this)(event.payload);
+    }.bind(this);
+}
+function getOutput() {
+    if (this.outputStream === undefined) {
+        this.outputStream = F.stream();
+    }
+    return this.outputStream;
+}
+function push() {
+    this.getOutput().push.apply(this.getOutput(), arguments);
+}
+function namedEvent(name) {
+    return function (event) {
+        this.push({
+            name: name,
+            payload: event
+        });
+    }.bind(this);
+}
